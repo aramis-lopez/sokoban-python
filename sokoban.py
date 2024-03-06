ca55a8a9ac52ca181983b328a966831bcf47566a
aramis-lopez <145937201+aramis-lopez@users.noreply.github.com>
2024-03-05 21:35:42 -0600

    Se agrego validaciones movimiento izquierda. - Se agrego funcion para mover cajas a la izquierda. - Se agregaron validaciones cuando estamos en personaje/meta - Se agrego validacion para mover caja a meta.

--- sokoban.py
+++ sokoban.py
@@ -10,13 +10,13 @@ class Sokoban:
             [3,3,3,3,3,3,3,3,3,3],
             [3,4,4,4,4,4,4,4,4,3],
             [3,4,4,4,4,4,4,4,4,3],
-            [3,4,4,4,4,4,4,4,4,3],
-            [3,4,4,4,0,4,4,4,4,3],
+            [3,4,4,4,2,1,4,4,4,3],
+            [3,4,4,4,4,4,4,0,4,3],
             [3,3,3,3,3,3,3,3,3,3]
         ]
 
         #Posicion del jugador
-        self.personaje_columna = 4
+        self.personaje_columna = 7
         self.personaje_fila = 4
 
         #Definir posicion del jugador
@@ -26,16 +26,40 @@ class Sokoban:
         for fila in self.mapa:
             print(fila)
 
+    def mover_izquierda_caja(self):
+        if(self.mapa[self.personaje_fila][self.personaje_columna -2]) == 4: #Validar si tienes pasillo alado de la caja
+            if(self.mapa[self.personaje_fila][self.personaje_columna]) == 5: #Validar si esta en personaje_meta
+                #[posicion_actual, posicion_destino, caja_destino]
+                self.mapa[self.personaje_fila][self.personaje_columna], self.mapa[self.personaje_fila][self.personaje_columna -1], self.mapa[self.personaje_fila][self.personaje_columna - 2] = 2, 0, 1
+                self.personaje_columna -= 1
+            else: #Personaje no esta en meta, mover caja
+                #[posicion_actual, posicion_destino, caja_destino]
+                self.mapa[self.personaje_fila][self.personaje_columna], self.mapa[self.personaje_fila][self.personaje_columna -1], self.mapa[self.personaje_fila][self.personaje_columna - 2] = 4, self.mapa[self.personaje_fila][self.personaje_columna], 1
+                self.personaje_columna -= 1
+        elif(self.mapa[self.personaje_fila][self.personaje_columna -2]) == 2: #Validar si tenemos meta alado de la caja
+            #[posicion_actual, posicion_destino, caja_destino]
+            self.mapa[self.personaje_fila][self.personaje_columna], self.mapa[self.personaje_fila][self.personaje_columna -1], self.mapa[self.personaje_fila][self.personaje_columna - 2] = 4, self.mapa[self.personaje_fila][self.personaje_columna], 6
+            self.personaje_columna -= 1
+        else: #Pared alado de la caja
+            print('No se puede mover la caja, tienes una pared.')
+            return self.mapa
+
+
     def mover_izquierda(self):
-        if self.mapa[self.personaje_fila][self.personaje_columna - 1] == 4: 
-                print("el personaje no se puede mover hacia la izquierda. Llegaste al limite del mapa.")
-                return self.mapa
-        else:
-            if self.mapa[self.personaje_fila][self.personaje_columna -1] == 3: #Valida si tienes pared
-                return self.mapa
-            else:
+        if self.mapa[self.personaje_fila][self.personaje_columna - 1] == 4: #Validar si hay pasillo
+                #Movemos personaje a la izquierda
                 self.mapa[self.personaje_fila][self.personaje_columna], self.mapa[self.personaje_fila][self.personaje_columna -1] = self.mapa[self.personaje_fila][self.personaje_columna -1], self.mapa[self.personaje_fila][self.personaje_columna]
                 self.personaje_columna -= 1
+        elif self.mapa[self.personaje_fila][self.personaje_columna -1] == 1: #Valida si tienes caja
+            self.mover_izquierda_caja()
+        elif self.mapa[self.personaje_fila][self.personaje_columna -1] == 6: #Valida si tienes caja_meta
+            if(self.mapa[self.personaje_fila][self.personaje_columna -2]) == 4: #Validar si tienes pasillo alado de la caja_meta
+                #[posicion_actual, posicion_destino, caja_meta-destino]
+                self.mapa[self.personaje_fila][self.personaje_columna], self.mapa[self.personaje_fila][self.personaje_columna -1], self.mapa[self.personaje_fila][self.personaje_columna - 2] = 4, 5, 1
+                self.personaje_columna -= 1
+        else:
+            print('No se puede mover al jugar, se llego al limite del mapa')
+            return self.mapa
 
 
     def mover_personaje(self, direccion):
